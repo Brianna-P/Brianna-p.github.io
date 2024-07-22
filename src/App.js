eac2d6b45b0d2a4490e1a29234be84e3ad6c9629
Brianna Patten <brianna.patten@ou.edu>
2024-07-21 19:19:16 -0500

    removed router bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Contact from "./pages/Contact";
