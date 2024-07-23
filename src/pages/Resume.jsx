696e2b18a75a69ee4e235e08c57920d20babfbd0
Brianna Patten <brianna.patten@ou.edu>
2024-07-22 21:55:14 -0500

    PDF error handle

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import resume from '../assets/resume.pdf';
 
 function Resume() {
+  const [error, setError] = useState(false);
+
+  const handleLoadError = () => {
+    setError(true);
+  };
+
   return (
     <div>
-      <embed src={resume} width="100%" height="1000px" />
+      {!error ? (
+        <embed src={resume} width="100%" height="1000px" onError={handleLoadError} />
+      ) : (
+        <p>PDF could not be displayed. Please be sure to open on non-mobile browser, or try with a different browser.</p>
+      )}
     </div>
   );
 }
