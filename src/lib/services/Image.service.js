a91130e490207a6ad976e5e81da443e171d0114d
Brianna Patten <brianna.patten@ou.edu>
2024-08-29 16:45:50 -0500

    Added new photo

--- src/lib/services/Image.service.js
+++ src/lib/services/Image.service.js
@@ -1,11 +1,15 @@
 import ImageModel from "../models/Image.model";
 import hacklahoma2023 from "../../assets/hacklahoma2023.jpg";
 import firstOUgame from "../../assets/firstOUgame.jpg";
+import intern from "../../assets/intern.jpg";
+
 
 
 const getAllImages = () => {
     const image1 = new ImageModel("OU's Hacklahoma, 2023", hacklahoma2023);
     const image2 = new ImageModel("My first OU Football game, 2024", firstOUgame);
+    const image3 = new ImageModel("Interning at Tinker AFB, 2024", intern);
+
 
 
     return [image1, image2];
